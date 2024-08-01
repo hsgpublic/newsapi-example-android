1e9fe4f5f6f4602ee8e39193c115361a810177fb
구홍석 <hsgpublic@gmail.com>
2024-08-01 12:40:07 +0900

    Headline data model 정의.

---
+++ app/src/main/java/hsgpublic/example/newsapi/data/model/HeadlineModel.kt
@@ -0,0 +1,9 @@
+package hsgpublic.example.newsapi.data.model
+
+data class HeadlineModel(
+    val title: String,
+    val publishedAt: String,
+    val author: String,
+    val urlToImage: String,
+    val url: String
+)
