2fc8433dd76fb98d20be1270dab67a51bcc840cd
구홍석 <hsgpublic@gmail.com>
2024-08-01 23:09:33 +0900

    TopHeadlinesRemoteDataSource 구현.

--- app/src/main/java/hsgpublic/example/newsapi/data/remote/TopHeadlinesRemoteDataSource.kt
+++ app/src/main/java/hsgpublic/example/newsapi/data/remote/TopHeadlinesRemoteDataSource.kt
@@ -1,10 +1,15 @@
 package hsgpublic.example.newsapi.data.remote
 
+import hsgpublic.example.newsapi.data.remote.endpoint.TopHeadlinesEndpoint
 import hsgpublic.example.newsapi.data.remote.helper.HttpRequester
 import hsgpublic.example.newsapi.data.remote.helper.RetrofitHelper
+import hsgpublic.example.newsapi.data.remote.model.TopHeadlinesResponseModel
 
 class TopHeadlinesRemoteDataSource(
     private val httpRequester: HttpRequester = RetrofitHelper
 ) {
-
+    suspend fun getTopHeadlines(country: String): TopHeadlinesResponseModel {
+        val endpoint = TopHeadlinesEndpoint(country = country)
+        return httpRequester.request(endpoint) as TopHeadlinesResponseModel
+    }
 }
