61a9d776fe7cdf3b598ec00926034f7a7afce8a5
구홍석 <hsgpublic@gmail.com>
2024-08-03 00:22:35 +0900

    Top headlines date time format.

--- app/src/main/java/hsgpublic/example/newsapi/util/StringExtension.kt
+++ app/src/main/java/hsgpublic/example/newsapi/util/StringExtension.kt
@@ -3,13 +3,15 @@ package hsgpublic.example.newsapi.util
 import java.time.LocalDateTime
 import java.time.format.DateTimeFormatter
 import java.time.format.FormatStyle
+import java.util.TimeZone
 
 fun String.currentLocaledDateString(fromFormat: String): String {
     try {
         val fromFormatter = DateTimeFormatter.ofPattern(fromFormat)
         val dateTime = LocalDateTime.parse(this, fromFormatter)
         val toFormatter = DateTimeFormatter.ofLocalizedDateTime(FormatStyle.MEDIUM)
-        val str = dateTime.format(toFormatter)
+        val toZone = TimeZone.getDefault().toZoneId()
+        val str = dateTime.atZone(toZone).format(toFormatter)
         return str
     } catch (e: Exception) {
         e.printStackTrace()
