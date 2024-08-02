eb3ce279bfd3158fbae00742eeaa75c76ff39668
구홍석 <hsgpublic@gmail.com>
2024-08-02 14:39:44 +0900

    TopHeadlinesRepository에 data source 연결.

--- app/src/main/java/hsgpublic/example/newsapi/data/remote/TopHeadlinesRemoteDataSource.kt
+++ app/src/main/java/hsgpublic/example/newsapi/data/remote/TopHeadlinesRemoteDataSource.kt
@@ -3,15 +3,19 @@ package hsgpublic.example.newsapi.data.remote
 import hsgpublic.example.newsapi.data.remote.endpoint.TopHeadlinesEndpoint
 import hsgpublic.example.newsapi.data.remote.helper.HttpRequester
 import hsgpublic.example.newsapi.data.remote.helper.TopHeadlinesRequester
+import hsgpublic.example.newsapi.data.remote.helper.TopHeadlinesService
 import hsgpublic.example.newsapi.data.remote.model.TopHeadlinesResponseModel
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.flow
 
 class TopHeadlinesRemoteDataSource(
     private val httpRequester: HttpRequester = TopHeadlinesRequester()
 ) {
-    suspend fun getTopHeadlines(country: String): TopHeadlinesResponseModel {
-        val endpoint = TopHeadlinesEndpoint(country = country)
-        return httpRequester.request(
-            endpoint = endpoint
-        ) as TopHeadlinesResponseModel
+    fun getTopHeadlines(country: String): Flow<TopHeadlinesResponseModel> {
+        return flow {
+            val endpoint = TopHeadlinesEndpoint(country = country)
+            val topHeadlines = httpRequester.request(endpoint) as TopHeadlinesResponseModel
+            emit(topHeadlines)
+        }
     }
 }
--- app/src/main/java/hsgpublic/example/newsapi/data/repository/TopHeadlinesRepository.kt
+++ app/src/main/java/hsgpublic/example/newsapi/data/repository/TopHeadlinesRepository.kt
@@ -1,4 +1,48 @@
 package hsgpublic.example.newsapi.data.repository
 
-class TopHeadlinesRepository {
+import hsgpublic.example.newsapi.data.model.HeadlineModel
+import hsgpublic.example.newsapi.data.remote.TopHeadlinesRemoteDataSource
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.collect
+import kotlinx.coroutines.flow.flow
+import kotlinx.coroutines.flow.last
+import kotlinx.coroutines.flow.map
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.runBlocking
+
+class TopHeadlinesRepository(
+    private val remoteDataSource: TopHeadlinesRemoteDataSource = TopHeadlinesRemoteDataSource(),
+    private val refreshIntervalMs: Long = 100
+) {
+    private var _headlines: List<HeadlineModel> = listOf()
+    val headlines: Flow<List<HeadlineModel>> = flow {
+        while(true) {
+            emit(_headlines)
+            delay(refreshIntervalMs)
+        }
+    }
+
+    fun fetchTopHeadlines(country: String) {
+        runBlocking {
+            try {
+                this.launch {
+                    _headlines = remoteDataSource.getTopHeadlines(country)
+                        .last()
+                        .articles
+                        .map { article ->
+                            HeadlineModel(
+                                title = article.title,
+                                publishedAt = article.publishedAt,
+                                author = article.author,
+                                urlToImage = article.urlToImage,
+                                url = article.url
+                            )
+                        }
+                }
+            } catch (e: Exception) {
+                throw e
+            }
+        }
+    }
 }
